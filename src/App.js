90178811e6c3ab25751b2e592fe925eb7bb43eec
nasantiago <santiago.nicholas.a@gmail.clmgit config --global user.email santiago.nicholas.a@gmail.com>
2019-11-02 23:00:43 -0400

    im trying again

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import React from 'react';
 //import logo from './logo.svg';
 //import './App.css';
-import Header from './components/header'
-import MainContent from './components/MainContent'
+//import Header from './components/header'
+import { Router, Route } from 'react-router';
+import mainPortal from './assets/images/greatSymbolImage.jpg'
+import African from './components/African Religions'
+import Atheism from './components/Atheism'
+import Buddhism from './components/Buddhism'
+import Christianity from './components/Christianity'
+import Hinduism from './components/Hinduism'
+import Islam from './components/Islam'
+import Judaism from './components/Judaism'
+import NativeAmericanReligion from './components/NativeAmericanReligion'
+import Shintoism from './components/Shintoism'
+import Sikhism from './components/Sikhism'
+import Taoism from './components/Taoism'
 
-function App() {
+class App extends React.Component {
+  render() {
   return (
     <div className="App">
-      <Header />
-      <MainContent />
+      <img src={mainPortal} alt="Main Portal" />
+      <Router>
+        <Route path = {"african"} component = {African}/>
+        <Route path = {"atheism"} component = {Atheism} />
+        <Route path = {"buddhism"} component = {Buddhism}/>
+        <Route path = {"christianity"} component = {Christianity}/>
+        <Route path = {"hinduism"} component = {Hinduism}/>
+        <Route path = {"islam"} component = {Islam}/>
+        <Route path = {"judaism"} component = {Judaism}/>
+        <Route path = {"native american"} component = {NativeAmericanReligion}/>
+        <Route path = {"shintoism"} component = {Shintoism}/>
+        <Route path = {"sikhism"} component = {Sikhism}/>
+        <Route path = {"taoism"} component = {Taoism}/>
+
+      </Router>
       
     </div>
   );
+}
 }
 
 export default App;
